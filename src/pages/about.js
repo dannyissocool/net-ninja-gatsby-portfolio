460d92388732bf8c524d83ff4033046f4a67ff75
Danny Meuse <danny.meuse@gmail.com>
2021-09-04 19:23:35 -0700

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          rerum praesentium accusantium voluptate tempore dignissimos eum saepe
+          adipisci magni ad.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          rerum praesentium accusantium voluptate tempore dignissimos eum saepe
+          adipisci magni ad.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          rerum praesentium accusantium voluptate tempore dignissimos eum saepe
+          adipisci magni ad.
+        </p>
+      </div>
+    </Layout>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,37 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
+import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
+import Img from "gatsby-image"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  console.log(data)
+
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Design & Develop</h3>
+          <p>UX designer & web developer based in Oregon</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
+        </div>
+        <Img fluid={data.file.childImageSharp.fluid} />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  query Banner {
+    file(uid: {}, relativePath: { eq: "selfie1.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
